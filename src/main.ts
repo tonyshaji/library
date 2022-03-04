12c0218f2d2e2d9a958ddb5138a6a5fd12b10c91
tonyshaji <tonys@thomsuninfocare.com>
2022-03-04 18:21:58 +0530

    swagger and hot reload added

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,28 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  const options = new DocumentBuilder()
+    .setTitle('Library example')
+    .setDescription('The Library API description')
+    .setVersion('1.0')
+    .addTag('User')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000, '0.0.0.0');
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();

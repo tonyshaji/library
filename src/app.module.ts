3fa69b96b245450aebadc47e68aaae2dafc05656
tonyshaji <tonys@thomsuninfocare.com>
2022-03-04 18:22:15 +0530

    book module empty pages

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forRoot('mongodb://localhost:27017/library'),
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
